e204579f5952bb1786c4909e433cd6d4bae0262d
Rayane <ninoucat11@gmail.com>
2023-08-03 15:52:27 -0400

    first commit

--- intervals/script.js
+++ intervals/script.js
@@ -3,3 +3,9 @@ let time = 0;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
 
+setInterval(updateTime, 1000)
+
+function updateTime(){
+    time += 1 ;
+    counterElement.innerHTML = time;
+}
